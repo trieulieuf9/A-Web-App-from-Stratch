a3e129dfab57267838ac7e138448bc94559b4800
Nguyen Ngoc Xuan Trieu <trieulieuf9@gmail.com>
2024-10-12 22:33:03 +0700

    Update main.py
    Finish the basic of a Web Server.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import socket
 
-## TODO:
-## 1. write a Request class, that parse a raw HTTP request string into action, path, headers, body, params
-## 2. write a Server class, that can take many requests without quitting run infinitely, only stop when receive a kill signal.
 
-class Response:  # A red room is also a kind of room.
-    def __init__(self):
-        self.headers = {"Content-Type": "text/plain"}
-        self.body = ""
-        self.status_code = 200
+class Server:
+    def __init__(self, host="127.0.0.1", port=1111):
+        self.host = host
+        self.port = port
+
+    def listen(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))  # s.close()
+            s.listen()
+
+            while True:
+                connection, addr = s.accept()
+                with connection:
+                    data = connection.recv(1024)  # TODO: support request bigger than 1024 bytes
+                    raw_http_request = data.decode("utf-8")
+                    yield connection, raw_http_request
+
+
+class Response:
+    def __init__(self, status_code, body="", headers=dict()):
+        self.headers = headers  # {"Content-Type": "text/plain"}
+        self.body = body
+        self.status_code = status_code
         self.resp_http_codes = {200: "OK",  # see here for more https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
                                 201: "Created",
                                 401: "Unauthenticated",
@@ -36,28 +51,48 @@ class Response:  # A red room is also a kind of room.
         return self.to_string().encode("utf-8")
 
 
-response = Response()
-response.body = "hello"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('127.0.0.1', 1111))
-    s.listen()
 
-    for i in range(2):
-        print("connection", i)
-        connection, addr = s.accept()
 
-        with connection:
-            print('Connected by', addr)
+class Request:
 
-            data = connection.recv(1024)
-            request = data.decode("utf-8")
-            print("--- Request ---")
-            print(request)
-            print("--- Response ---")
-            print(response.to_string())
+    def __init__(self, raw_http_request):
+        self.raw_http_request = raw_http_request
+        lines = raw_http_request.split("\n")
+        parts = lines[0].split(" ")  # EX: "GET /example?hello HTTP/1.1"
+        self.action = parts[0]
+        self.full_path = parts[1]  # path and params
+        self.path = parts[1].split("?")[0]
+        self.http_protocol = parts[2]
+        self.params = self.parse_params(self.full_path)  # dict of keys and values
 
-            # conn.send(response_header)
-            # conn.send(content_length)
-            # conn.send(padding)
-            connection.sendall(response.to_buffer())
+
+    def get_path(self):
+        return ""
+
+    def parse_params(self, full_path):
+        # todo later
+        return dict()
+
+
+def main():
+    server = Server()
+    for connection, raw_http_request in server.listen():
+        print(raw_http_request)
+
+        request = Request(raw_http_request)
+
+        # request routing
+        if request.action == "GET" and (request.path == "/" or request.path == "/home" or request.path == "/index"):
+            response = home_action(request)
+        else:
+            response = Response(404, body="<h1>Not Found</h1>", headers={"Content-Type": "text/html"})
+
+        connection.sendall(response.to_buffer())
+
+
+def home_action(request):
+    return Response(200, body="<h1>Home page</h1>", headers={"Content-Type": "text/html"})
+
+
+main()
